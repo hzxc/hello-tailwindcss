2130f4f4e7ee47b642d4d9259fb5afc1d28960d5
Your Name <you@example.com>
2022-09-21 17:22:38 +0800

    2022-9-21 17:22

---
+++ pages/demo/chitchat.tsx
@@ -0,0 +1,27 @@
+import { NextPage } from 'next';
+import dayjs from 'dayjs';
+import Image from 'next/image';
+
+const ChitChat: NextPage = () => {
+  return (
+    <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
+      <div className='shrink-0'>
+        <div className='relative h-12 w-12'>
+          <Image
+            src={`https://robohash.org/${dayjs().unix()}`}
+            alt='ChitChat Logo'
+            layout='fill' // required
+            objectFit='cover' // change as you like
+            className='rounded-full' // you can use other classes here too
+          />
+        </div>
+      </div>
+      <div>
+        <div className='text-xl font-medium text-black'>ChitChat</div>
+        <p className='text-slate-500'>You have a new message!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChitChat;
---
+++ pages/demo/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Demo: NextPage = () => {
+  return <div>Demo</div>;
+};
+
+export default Demo;
