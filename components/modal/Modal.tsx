9dbf3ae157a34f6e5568d6243ffe40d896755868
Your Name <you@example.com>
2022-09-27 11:55:57 +0800

    2022-9-27 11:55

--- components/modal/Modal.tsx
+++ components/modal/Modal.tsx
@@ -1,10 +1,14 @@
 import { Portal } from 'components/portal';
+import { useToggle } from 'hooks/useToggle';
 import { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+import { ModalOverlay } from '../pancake/pan-modal/ModalOverlay';
 
-export const Modal: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <Portal>
-      <div className='absolute top-0 left-0 right-0 bottom-0 z-50'>{children}</div>
-    </Portal>
-  );
+export const Modal: React.FC<{ visible: boolean; children: ReactNode }> = ({
+  visible,
+  children,
+}) => {
+  const ModalDom = <div className='absolute top-0 left-0 right-0 bottom-0 z-50'>{children}</div>;
+  if (!visible) return null;
+  return <Portal>{ModalDom}</Portal>;
 };
