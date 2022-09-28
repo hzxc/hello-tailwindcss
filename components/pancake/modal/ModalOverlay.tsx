1a1aad8b0215c8f187c9be601e83f3de9daea6be
Your Name <you@example.com>
2022-09-29 02:03:41 +0800

    2022-9-29 02:03

--- components/pancake/modal/ModalOverlay.tsx
+++ components/pancake/modal/ModalOverlay.tsx
@@ -4,7 +4,7 @@ export const ModalOverlay: React.FC<HTMLAttributes<HTMLDivElement>> = (props) =>
   const { onClick, children, ...restProps } = { ...props };
   return (
     <div
-      className='absolute top-0 left-0 right-0 bottom-0 bg-purple-900/20'
+      className='absolute top-0 left-0 right-0 bottom-0 bg-blue-800/20'
       onClick={onClick}
       {...restProps}
     >
