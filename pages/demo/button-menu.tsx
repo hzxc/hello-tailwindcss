406790681623e96d65c20a30a0002fb3f2ac1776
Your Name <you@example.com>
2022-10-12 23:26:30 +0800

    2022-10-12 23:26

--- pages/demo/button-menu.tsx
+++ pages/demo/button-menu.tsx
@@ -9,36 +9,26 @@ import { Header } from 'components/pancake/layout/Header';
 
 const ButtonMenuDemo: NextPageWithLayout = () => {
   return (
-    <div className='p-8'>
-      <div className='flex flex-col items-center gap-4 grow-0 shrink-0'>
-        <MenuButton
-          navBtn={
-            <IconButton
-              className='align-middle'
-              tailClass='panMb'
-              leftSize='32px'
-              leftSrc='/images/pancake/56.png'
-              rightIcon={<ArrowDownSvg />}
-              customStyle={{ boxShadow: 'rgb(0 0 0 / 10%) 0px -2px 0px inset' }}
-            >
-              BNB Smart Chain
-            </IconButton>
-          }
-        >
-          <MenuButtonItem>BNB Smart Chain</MenuButtonItem>
-          <MenuButtonItem>Ethereum</MenuButtonItem>
-          <MenuButtonItem>BNB Smart Chain</MenuButtonItem>
-          <MenuButtonItem>Ethereum</MenuButtonItem>
-        </MenuButton>
-        <div className=''> hello</div>
-
-        {/* <Menu>
-        <SubMenu title={'test'}>
-          <MenuItem>hello</MenuItem>
-        </SubMenu>
-      </Menu> */}
-      </div>
-      <Header></Header>
+    <div className='flex p-8'>
+      <MenuButton
+        navBtn={
+          <IconButton
+            className='my-1'
+            tailClass='panMb'
+            leftSize='32px'
+            leftSrc='/images/pancake/56.png'
+            rightIcon={<ArrowDownSvg />}
+            customStyle={{ boxShadow: 'rgb(0 0 0 / 10%) 0px -2px 0px inset' }}
+          >
+            BNB Smart Chain
+          </IconButton>
+        }
+      >
+        <MenuButtonItem>BNB Smart Chain</MenuButtonItem>
+        <MenuButtonItem>Ethereum</MenuButtonItem>
+        <MenuButtonItem>BNB Smart Chain</MenuButtonItem>
+        <MenuButtonItem>Ethereum</MenuButtonItem>
+      </MenuButton>
     </div>
   );
 };
